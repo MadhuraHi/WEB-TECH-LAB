2946978ab840a9a60e9b812afe2bcd9c9c0b6d03
MadhuraHi <166099484+MadhuraHi@users.noreply.github.com>
2026-04-08 23:16:45 +0530

    LAB-12

---
+++ LAB-12/ex1/start.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Starting Node.js REST API Server...
+cd "C:\Users\MADHU\OneDrive\Desktop\WEB LAB GIT\LAB-12\ex1"
+node server.js
+pause
---
+++ LAB-12/ex3/start.bat
@@ -0,0 +1,6 @@
+@echo off
+title MongoDB CRUD Server
+echo Starting MongoDB CRUD Server...
+cd /d "C:\Users\MADHU\OneDrive\Desktop\WEB LAB GIT\LAB-12\ex3"
+node server.js
+pause
